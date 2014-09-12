8be52f9f324a18a9193f303800cf067de359e8d0
Hana Kim <hanakimis@gmail.com>
2014-09-12 00:18:20 -0700

    wip

---
+++ HanaKimWeek1/SignInRootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInRootViewController.swift
+//  HanaKimWeek1
+//
+//  Created by Hana Kim on 9/11/14.
+//  Copyright (c) 2014 Hana Kim. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("SignInSegue", sender: self)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
